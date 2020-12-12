100d1ea50a94aacdc0b0409400f0040547ccbddf
shystruk <v.stokol@gmail.com>
2020-12-12 10:38:41 -0800

    formatting

--- src/custom-event-js.js
+++ src/custom-event-js.js
@@ -4,58 +4,58 @@ var TARGET = document;
 var EVENTS = {};
 
 /**
- * @param {String} eventName 
+ * @param {String} eventName
  * @param {Object} detail
  */
 function _dispatchEvent(eventName, detail) {
-  var event = new CustomEvent(eventName, {
-    detail: detail
-  });
+	var event = new CustomEvent(eventName, {
+		detail: detail
+	});
 
-  TARGET.dispatchEvent(event);
+	TARGET.dispatchEvent(event);
 }
 
 function _CustomEventPolyfill() {
-  if (typeof window.CustomEvent === 'function') {
-    return;
-  }
-  
-  function CustomEvent(event, params) {
-    var evt = document.createEvent('CustomEvent');
+	if (typeof window.CustomEvent === 'function') {
+		return;
+	}
 
-    params = params || { bubbles: false, cancelable: false, detail: undefined };
-    evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
-    
-    return evt;
-  }
-  
-  CustomEvent.prototype = window.Event.prototype;
-  window.CustomEvent = CustomEvent;
+	function CustomEvent(event, params) {
+		var evt = document.createEvent('CustomEvent');
+
+		params = params || { bubbles: false, cancelable: false, detail: undefined };
+		evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+
+		return evt;
+	}
+
+	CustomEvent.prototype = window.Event.prototype;
+	window.CustomEvent = CustomEvent;
 }
 
 module.exports = {
-  /**
-   * @param {String} eventName 
-   * @param {Function} callback 
-   */
-  on: function(eventName, callback) {
-    EVENTS[eventName] = callback;
-    TARGET.addEventListener(eventName, callback);
-  },
+	/**
+	 * @param {String} eventName
+	 * @param {Function} callback
+	 */
+	on: function (eventName, callback) {
+		EVENTS[eventName] = callback;
+		TARGET.addEventListener(eventName, callback);
+	},
 
-  /**
-   * @param {String} eventName 
-   */
-  off: function(eventName) {
-    TARGET.removeEventListener(eventName, EVENTS[eventName]);
-    delete EVENTS[eventName];
-  },
+	/**
+	 * @param {String} eventName
+	 */
+	off: function (eventName) {
+		TARGET.removeEventListener(eventName, EVENTS[eventName]);
+		delete EVENTS[eventName];
+	},
 
-  /**
-   * @param {String} eventName 
-   * @param {Object} detail
-   */
-  dispatch: function(eventName, detail) {
-    _dispatchEvent(eventName, detail || null);
-  }
+	/**
+	 * @param {String} eventName
+	 * @param {Object} detail
+	 */
+	dispatch: function (eventName, detail) {
+		_dispatchEvent(eventName, detail || null);
+	}
 }
